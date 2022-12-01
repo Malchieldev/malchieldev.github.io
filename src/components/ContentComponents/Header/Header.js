a6b5116464011697a6ef2a035e2fffe42115f4c8
Malchiel Dev <kszolotuhin@gmail.com>
2022-12-01 15:14:22 +0300

    Added mobile nav button Fixed clicks on mobile versions on images (no more scaling) Replaced favicon Rewritten index file with more suitable descriptions and names

--- src/components/ContentComponents/Header/Header.js
+++ src/components/ContentComponents/Header/Header.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import styled from "./Header.module.css";
 
 const Header = (props) => {
+  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false);
   const [headerIsShown, setHeaderisShown] = useState(false);
   useEffect(() => {
     setHeaderisShown(true);
@@ -12,33 +13,45 @@ const Header = (props) => {
     ? styled.header
     : `${styled.header} ${styled.shown}`;
 
+  const mobileButtonHandler = () => {
+    setMobileMenuIsShown((prev) => !prev);
+  };
+
   return (
     <header className={headerClassName}>
-      <ul>
-        {props.titles.map((el, i) => {
-          return (
-            <button
-              className={
-                props.activeSection === el.title ? styled.activeSection : ""
-              }
-              onClick={() => {
-                props.scrollToSection(i);
-              }}
-              key={el.id}
-            >
-              {el.title}
-            </button>
-          );
-        })}
-        <button>
-          <a
-            href="CV/Software Engineer Zolotuhin Kirill CV.pdf"
-            target="_blank"
-          >
-            CV
-          </a>
+      <nav>
+        <button className={styled.mobile__activeSection}>
+          {props.activeSection}
         </button>
-      </ul>
+        <button className={styled.mobile__button} onClick={mobileButtonHandler}>
+          {'///'}
+        </button>
+        <ul className={mobileMenuIsShown ? styled.menu__shown : ""}>
+          {props.titles.map((el, i) => {
+            return (
+              <button
+                className={
+                  props.activeSection === el.title ? styled.activeSection : ""
+                }
+                onClick={() => {
+                  props.scrollToSection(i);
+                }}
+                key={el.id}
+              >
+                {el.title}
+              </button>
+            );
+          })}
+          <button>
+            <a
+              href="CV/Software Engineer Zolotuhin Kirill CV.pdf"
+              target="_blank"
+            >
+              CV
+            </a>
+          </button>
+        </ul>
+      </nav>
     </header>
   );
 };
